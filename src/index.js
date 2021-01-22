a4c36e343bc2a7304c2ddb1f909487e004bc6783
Karin Goh <karingoh@users.noreply.github.com>
2021-01-21 19:41:33 -0600

    test commit test description

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './css/App.css';
-import App from './components/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./css/App.css";
+import App from "./components/App";
 
 // render the App in to the DOM inside the element with id "root"
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// test comment
